9a4dfb1e343b7c6f72a43bc1cbdf6f10ba601a96
liel berniker <lielb101995@gmail.com>
2022-04-25 10:52:32 +0300

    add colors

--- stree.c
+++ stree.c
@@ -15,10 +15,14 @@
 #include <ftw.h>
 #include <errno.h>
 
+
+int dirs_counter=0;
+int files_counter =0;
 void mode_to_letters(int mode);
 int print_user_name_by_uid(uid_t uid);
 int print_group_by_gid(gid_t gid);
 int check_is_hidden(const char *pathname );
+int print_name(const char* name,int type);
 char *PATH;
 int ARGC = 0;
 
@@ -28,7 +32,9 @@ dirTree(const char *pathname, const struct stat *sbuf, int type, struct FTW *ftw
     if(check_is_hidden(pathname)== 1)return 0;
     if(strcmp(pathname,".")==0)
     {
+        printf("\033[0;34m");
         printf(".\n");
+        printf("\033[0m");
         return 0;
     }
     else if (ARGC > 1 && strcmp(pathname,PATH)==0)
@@ -63,13 +69,19 @@ dirTree(const char *pathname, const struct stat *sbuf, int type, struct FTW *ftw
     mode_to_letters(sbuf->st_mode);
     print_user_name_by_uid(sbuf->st_uid);
     print_group_by_gid(sbuf->st_gid);
-    printf("%ld]  ",(long)sbuf->st_size);
-    printf("%s\n" , filename);
+    printf("%15ld]  ",(long)sbuf->st_size);
+    print_name(filename,type);
+    if (type == FTW_F)
+        files_counter++;
+    if (type == FTW_D && strcmp(".", filename) != 0)
+        dirs_counter++;
     return 0;                                   /* Tell nftw() to continue */
 }
 int main(int argc, char *argv[])
 {
     int flags = 0;
+    char * dirs;
+    char * files; 
     if (argc < 2) 
     {
         if (nftw(".", dirTree, 10, flags) == -1) 
@@ -79,6 +91,15 @@ int main(int argc, char *argv[])
         }
         else
         {
+                        if (dirs_counter == 1)
+                dirs = "directory";
+            else
+                dirs = "directories";
+            if (files_counter == 1)
+                files = "file";
+            else
+                files = "files";
+            printf("\n%d %s, %d %s\n", dirs_counter, dirs, files_counter, files);
             exit(EXIT_SUCCESS);
         }
     }
@@ -159,13 +180,14 @@ void mode_to_letters(int mode)
 int print_user_name_by_uid(uid_t uid)
 {
     struct passwd *getpwuid(), *pw_ptr;
-    if((pw_ptr = getpwuid(uid) ) == NULL)
+    if ((pw_ptr = getpwuid(uid)) == NULL)
     {
-        printf("%d",uid);
+        printf("%d", uid);
         return 1;
     }
-    else{
-        printf("%s   ",pw_ptr->pw_name);
+    else
+    {
+        printf("%s\t ", pw_ptr->pw_name);
     }
     return 0;
 }
@@ -173,13 +195,14 @@ int print_user_name_by_uid(uid_t uid)
 int print_group_by_gid(gid_t gid)
 {
     struct group *getgrgid(), *grp_ptr;
-    if((grp_ptr = getgrgid(gid) ) == NULL)
+    if ((grp_ptr = getgrgid(gid)) == NULL)
     {
-        printf("%d",gid);
+        printf("%d", gid);
         return 1;
     }
-    else{
-        printf("%s          ",grp_ptr->gr_name);
+    else
+    {
+        printf("%s ", grp_ptr->gr_name);
     }
     return 0;
 }
@@ -197,228 +220,19 @@ int check_is_hidden(const char *pathname )
     }
     return 0;   
 }
+int print_name(const char* name,int type)
+{
+         if (type == FTW_D)
+            {printf("\e[1;94m");
+            }
+        else if (type == FTW_F)
+           {
+               if(strstr(name, ".zip"))
+               {printf("\e[1;91m");}
+               else if(!strstr(name, ".") && !strstr(name, "Makefile"))
+               {printf("\e[1;92m");}
+             }
+             printf("%s\n" , name);
+               printf("\033[0m");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <fcntl.h>
-// #include <unistd.h>
-// #include <string.h>
-// #include <time.h>
-// #include <limits.h>
-// #include <sys/stat.h>
-// #include <sys/types.h>
-// #include <pwd.h>
-// #include <grp.h>
-// #include <dirent.h>
-// #include <sys/stat.h>
- 
-// size_t dir_count =0;
-// size_t files_count =0;
-// int print_file_details(char * filename, struct stat *info_p,int space_count);
-// int dir_iterate(char *dirname, int space_count);
-// void dostat(char *filename ,int space_count);
-// void mode_to_letters(int mode);
-// int print_user_name_by_uid(uid_t uid);
-// int print_group_by_gid(gid_t gid);
-// int main(int argc, char * argv[])
-// {
-//     char dir_name[PATH_MAX];
-//     if(argc > 1)
-//     {
-//         //dir_name = argv[1]; 
-//     }
-//     else
-//     {
-//         if(getcwd(dir_name,sizeof(dir_name))!=NULL)
-//         {
-//             int spaces = 0;
-//             int ans = dir_iterate(dir_name,spaces);
-//         }
-//         else
-//         {
-//             return -1;
-//         }
-//     }
-//     //printf("\n%zu directories, %zu files\n", dir_count - 1, files_count);
-//     return 0;
-// }
-
-// int dir_iterate(char *dirname, int space_count)
-// {
-//     //NOTE - we should convert dirname to be the full path of this file.
-//     DIR *dir_ptr;
-//     struct dirent *direntp;
-//     struct stat *temp_stat;
-//     char DirName[PATH_MAX];
-//     getcwd(DirName,sizeof(DirName));
-//     //NOTE - we should check if the current file/directory is an hidden file/the original directory of the execution.
-//     if(strcmp(dirname , DirName)!=0 && strcmp(dirname , "..")!=0 && strcmp(dirname , ".")!=0 && strcmp(dirname , ".git")!=0)
-//     {
-//         dostat(dirname,space_count);
-//     }
-//     if((dir_ptr = opendir( dirname )) == NULL)
-//     {
-//         return 0;
-//     }
-//     dir_count++;
-
-//     //NOTE - this while loop are iterate over hidden files/directories such as - "." , ".." , ".git" etc.
-//     while ((direntp= readdir(dir_ptr)) != NULL)
-//     {
-//         if(direntp->d_type!=4)
-//         {
-//             files_count++;
-//         }
-//         dir_iterate(direntp->d_name,space_count+1);
-//     }
-//     return 0;
-// }
-
-// void dostat(char *filename,int space_count)
-// {
-//     struct stat info;
-//     if( (stat(filename,&info))== -1)
-//     {
-//         perror(filename);
-//     }
-//     else
-//     {
-//         //printf("%s\n" , filename);
-//         print_file_details(filename,&info,1);
-//     }
-// }
-
-// int print_file_details(char * filename, struct stat *info_p,int space_count)
-// {
-//     for (size_t i = 0; i < space_count; i++)
-//     {
-//         printf("    ");
-//     }
-//     printf("|___ [");
-//     mode_to_letters(info_p->st_mode);
-//     print_user_name_by_uid(info_p->st_uid);
-//     print_group_by_gid(info_p->st_gid);
-//     printf("%ld]  ",(long)info_p->st_size);
-//     printf("%s\n",filename);
-//     return 0;
-// }
-// void mode_to_letters(int mode)
-// {
-//     char *str;
-//     strcpy(str,"---------- ");
-//     if(S_ISDIR(mode)){
-//         str[0] = 'd';
-//     }
-//     if(S_ISCHR(mode)){
-//         str[0] = 'c';
-//     }
-//     if(S_ISBLK(mode)){
-//         str[0] = 'b';
-//     }
-
-//     if(mode & S_IRUSR){
-//         str[1] = 'r';
-//     }
-//     if(mode & S_IWUSR){
-//         str[2] = 'w';
-//     }
-//     if(mode & S_IXUSR){
-//         str[3] = 'x';
-//     }
-
-//     if(mode & S_IRGRP){
-//         str[4] = 'r';
-//     }
-//     if(mode & S_IWGRP){
-//         str[5] = 'w';
-//     }
-//     if(mode & S_IXGRP){
-//         str[6] = 'x';
-//     }
-
-//     if(mode & S_IROTH){
-//         str[1] = 'r';
-//     }
-//     if(mode & S_IWOTH){
-//         str[2] = 'w';
-//     }
-//     if(mode & S_IXOTH){
-//         str[3] = 'x';
-//     }
-
-//     printf("%s",str);
-// }
-// int print_user_name_by_uid(uid_t uid)
-// {
-//     struct passwd *getpwuid(), *pw_ptr;
-//     if((pw_ptr = getpwuid(uid) ) == NULL)
-//     {
-//         printf("%d",uid);
-//         return 1;
-//     }
-//     else{
-//         printf("%s   ",pw_ptr->pw_name);
-//     }
-//     return 0;
-// }
-
-// int print_group_by_gid(gid_t gid)
-// {
-//     struct group *getgrgid(), *grp_ptr;
-//     if((grp_ptr = getgrgid(gid) ) == NULL)
-//     {
-//         printf("%d",gid);
-//         return 1;
-//     }
-//     else{
-//         printf("%s          ",grp_ptr->gr_name);
-//     }
-//     return 0;
-// }
+}
